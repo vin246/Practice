d4aed7d6d6ab9deeb3b92a8513fe364394881cb2
dasar <dasar@NAVYUGA>
2022-03-17 15:43:46 +0530

    commit2

--- src/test/java/practice/DemoTest.java
+++ src/test/java/practice/DemoTest.java
@@ -3,6 +3,6 @@ package practice;
 public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("First Line Of Code to GitHub");
-		System.out.println("Second Line from Oxygen");
+		System.out.println("Second Line from Oxygen ");
 	}
 }
